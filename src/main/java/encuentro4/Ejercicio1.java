9ca5cb114e2be9f4dae02392a7557e47d79d6e9b
ramiro galvan <ramiro_coronel333@hotmail.com>
2025-07-29 17:10:26 -0300

    encuentro4 ejercicio1 - resuelto

--- src/main/java/encuentro4/Ejercicio1.java
+++ src/main/java/encuentro4/Ejercicio1.java
@@ -1,4 +1,38 @@
 package encuentro4;
 
+import com.github.sanchezih.util.io.Consola;
+
+/**
+ * Ejercicio01
+ * Programa que pide el ingreso de un numero e indica si es un numero primo o no.
+ */
 public class Ejercicio1 {
+    public static void main(String[] args) {
+        int numero;
+
+        System.out.println("Ingrese un numero:");
+        numero = Consola.leerEntero();
+
+        //verificar que sea un numero primo
+        boolean primo = esPrimo(numero);
+        System.out.println("El numero " + numero + (primo ? " es primo" : " no es primo"));
+    }
+
+    private static boolean esPrimo(int numero) {
+        if (numero <= 1) {
+            return false;
+        }
+
+        int cantidadDivisores = 0;
+        int i = 1;
+
+        while (i <= numero && cantidadDivisores <= 2) {
+            if (numero % i == 0) {
+                cantidadDivisores++;
+            }
+            i++;
+        }
+        return cantidadDivisores == 2;
+    }
+
 }
